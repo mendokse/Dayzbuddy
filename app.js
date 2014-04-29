eb99211f71423895208ea5b5ca8750959f7b409e
Mendokse <joakimodelvall@Joakims-MacBook-Pro.local>
2014-04-29 17:05:51 +0200

    magnus hjälp

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ var express = require('express'),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 	
-server.listen(process.env.PORT || 9000);
-
-app.configure(function () {
-    app.set('port', process.env.PORT || 9000);
-    app.use(express.static(__dirname + '/public'));
-});
-
-// server.listen(9000);
+// server.listen(process.env.PORT || 9000);
 
 // app.configure(function () {
-//     app.set('port', 9000);
+//     app.set('port', process.env.PORT || 9000);
 //     app.use(express.static(__dirname + '/public'));
 // });
+
+server.listen(9000);
+
+app.configure(function () {
+    app.set('port', 9000);
+    app.use(express.static(__dirname + '/public'));
+});
 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + '/index.html');
@@ -56,45 +56,42 @@ function GetMeetLocation(){
 
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
-		var rooms = io.sockets.manager.rooms,
-			room;
+		var rooms = io.sockets.manager.rooms;
 
 		socket.username = data;
 
-		if (rooms) {
-			for (roomName in rooms) {
-				roomName = roomName.substring(1);
+		if ('/lobby' in rooms && io.sockets.clients('lobby') && io.sockets.clients('lobby').length === 1) {
+			socket.room = (Math.random() + 1).toString(36).substring(2);
 
-				if (io.sockets.clients(roomName) && io.sockets.clients(roomName).length === 1) {
-					room = roomName;
+			socket.join(socket.room);
+			console.log(socket.username + ' joined room ', socket.room);
 
-					socket.room = room;
-					socket.join(room);
-					console.log('hest');
-					io.sockets.in(socket.room).emit('match found',{location: GetMeetLocation()});
-					callback("YOLO");
+			io.sockets.in('lobby').emit('move player', {room: socket.room});
+		} else {
+			socket.join('lobby');
 
-
-
-					return;
-				}
-			}
+			io.sockets.in('lobby').emit('waiting for match', {});
 		}
-
-		if(!room) {
-			room = (Math.random() + 1).toString(36).substring(2);
-		}
-
-		socket.room = room;
-		socket.join(room);
+		
 		callback("YOLO");
 		return;
 	});
 
-	socket.on('new message', function(data){
-		
-		io.sockets.in(socket.room).emit('send message', {msg: data, username: socket.username});
+	socket.on('move player', function(data){
+		socket.leave('lobby');
+		socket.join(data.room);
 
+		console.log('moved ' + socket.username + ' to room -> ', data.room);
+
+		io.sockets.in(data.room).emit('match found', {room: room});
+	});
+
+	socket.on('match found', function(data){
+		io.sockets.in(data.room).emit('broadcast', {location: GetMeetLocation()});
+	});
+
+	socket.on('new message', function(data){
+		io.sockets.in(socket.room).emit('send message', {msg: data, username: socket.username});
 	});
 	
 });
